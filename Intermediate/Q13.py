a2504a219ff3324832df3e19fca04bc31a358403
Shubham Jadhav <shubhamj0828@gmail.com>
2023-11-24 18:33:32 -0500

    Q13 Done

---
+++ Intermediate/Q13.py
@@ -0,0 +1,7 @@
+check_startswith = lambda input_string, given_char: input_string.startswith(given_char)
+
+input_string = "Hello, World!"
+given_char = "H"
+
+result = check_startswith(input_string, given_char)
+print("Output:", result)
